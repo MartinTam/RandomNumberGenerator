797a2364a5c0892de15486b5161dc0adc5dbb1ff
MartinTam <hongvtam@protonmail.com>
2021-03-10 16:39:32 +0100

    Basic frontend done

--- randomNumber.py
+++ randomNumber.py
@@ -2,32 +2,34 @@ from tkinter import *
 import random
 
 root = Tk()
-root.geometry('800x500')
+root.geometry('450x500')
 root.title('Random Number Generator')
 # ---------------------------------------------------------------
 
+MOVE = 30
+
 # Title
-title = Label(root, text = 'Random Number Generator', font = ('Comicsans', 20)).grid(row = 0, column = 0)
+title = Label(root, text = 'Random Number Generator', font = ('Comicsans', 20), padx = MOVE, pady = MOVE).grid(row = 0, column = 1, columnspan = 4)
 
 # Type of number
 chooseType = StringVar()
 chooseType.set('Integer')
-Radiobutton(root, text = 'Integer', variable = chooseType, value = 'Integer').grid(row = 1, column = 0)
-Radiobutton(root, text = 'Floating point', variable = chooseType, value = 'Floating point').grid(row = 2, column = 0)
+Radiobutton(root, text = 'Integer', variable = chooseType, value = 'Integer', padx = MOVE).grid(row = 1, column = 1, columnspan = 2)
+Radiobutton(root, text = 'Floating point', variable = chooseType, value = 'Floating point', padx = MOVE).grid(row = 2, column = 1, columnspan = 2)
 
 # Values
-startTitle = Label(root, text = 'From:').grid(row = 3, column = 0)
-endTitle = Label(root, text = 'To:').grid(row = 3, column = 1)
+startTitle = Label(root, text = 'From:' ).grid(row = 3, column = 1, padx = MOVE, pady = (MOVE, 0))
+endTitle = Label(root, text = 'To:' ).grid(row = 3, column = 2, padx = MOVE, pady = (MOVE, 0))
 
 startValue = Entry(root, width = 20)
 endValue = Entry(root, width = 20)
 
-startValue.grid(row = 4, column = 0)
-endValue.grid(row = 4, column = 1)
+startValue.grid(row = 4, column = 1, padx = MOVE, pady = (0, MOVE) )
+endValue.grid(row = 4, column = 2, padx = MOVE, pady = (0, MOVE) )
 
 # Generate random number
 
-showRandomNumber = Label(root)  # For destroying the last generate number
+showRandomNumber = Label(root, padx = MOVE, pady = MOVE)  # For destroying the last generate number in the background
 
 def randomNumber():
     global showRandomNumber
@@ -35,30 +37,19 @@ def randomNumber():
 
     if chooseType.get() == 'Integer':
         number = random.randint( int( startValue.get() ), int( endValue.get() ) )
-        showRandomNumber = Label(root, text = number)
-        showRandomNumber.grid(row = 6, column = 0)
+        showRandomNumber = Label(root, text = number, padx = MOVE, pady = MOVE)
+        showRandomNumber.grid(row = 6, column = 1, columnspan = 2)
 
     
     elif chooseType.get() == 'Floating point':
         number = random.uniform( float( startValue.get() ), float( endValue.get() ) )
-        showRandomNumber = Label(root, text = number)
-        showRandomNumber.grid(row = 6, column = 0)
+        showRandomNumber = Label(root, text = number, padx = MOVE, pady = MOVE)
+        showRandomNumber.grid(row = 6, column = 1, columnspan = 2)
 
-
-    #showRandomNumber.grid_forget()
 
 # Generate button
-generate = Button(root, text = 'Generate', command = randomNumber).grid(row = 5, column = 0, columnspan = 2)
+generate = Button(root, text = 'Generate', command = randomNumber).grid(row = 5, column = 1, columnspan = 2, padx = MOVE, pady = (0, MOVE) )
 
 
 # ---------------------------------------------------------------
 root.mainloop()
-
-
-
-
-
-#number = random.randint(9,9)
-#number = random.uniform(10, 1)
-
-#print(number)
